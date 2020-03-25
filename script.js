62e76353a76df11cb8c052c6c2669660db85c43c
ALeks <arokokko@gmail.com>
2020-03-25 16:25:05 +0700

    Promise created

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 
 let inputRub = document.getElementById('rub'),
-    inputUsd = document.getElementById('usd');
+inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', (e) => {
-    if (isNaN(e.target.value)) {
-        inputUsd.value = "Input a number";    //проверка на ввод чисел
-        return false;
+    
+    function catchData() {
+
+        return new Promise(function(resolve, reject){
+            let request = new XMLHttpRequest();
+            request.open("GET", "current.json");
+        
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+        
+            request.onload = function() {
+                if(request.readyState === 4) {
+                        if(request.status == 200) {
+                            resolve(this.response);
+                        }
+                        else {
+                            reject();
+                        
+                        }
+                }
+            };
+        });
     }
-    let request = new XMLHttpRequest();
 
-    // request.open(method, url- путь к серверу, async - по умолчанию true, login, password);
-
-    request.open('GET', 'current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8' );
-    request.send();
-
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
-
+    catchData()
+        .then(response => {
+            console.log(response);
+            let data = JSON.parse(response);
             inputUsd.value = inputRub.value / data.usd;
-        }
-        else {
-            inputUsd.value = "Something going wrong!";
-        }
-    });
+        })
+        .then(() => console.log(5000))
+        .catch(() => inputUsd.value = "Что-то пошло не так");
+
+
 });
